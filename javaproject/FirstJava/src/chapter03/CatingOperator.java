5dcff833bb93532d69ba1bdf837d77f0df00bc5d
ByXavi33 <byxavi33@gmail.com>
2022-09-14 17:04:33 +0900

    단일 연산자
    단일 연산

---
+++ javaproject/FirstJava/src/chapter03/CatingOperator.java
@@ -0,0 +1,19 @@
+package chapter03;
+
+public class CatingOperator {
+
+	public static void main(String[] args) {
+		
+		byte num1 = 10;
+		byte num2 = 20;
+		
+		// 기본형 / 참조형
+		// 기본형 
+		// 숫자 문자 논리값
+		// 정수 실수 문자 논리값
+		// byte short int long, float doule, char, boolaen
+		
+		byte result = num1 + num2; 
+		//byte + byte => int + int = int
+	}
+}
