a25a5e5b330130272f36c272d88be43e0ba12446
ByXavi33 <byxavi33@gmail.com>
2022-09-15 10:37:20 +0900

    Create RelationalOperator.java

---
+++ javaproject/FirstJava/src/chapter03/RelationalOperator.java
@@ -0,0 +1,27 @@
+package chapter03;
+
+public class RelationalOperator {
+
+	public static void main(String[] args) {
+		
+		// 관계연산자 ( 비교연산자 )
+		// > < >= <= == !=
+		
+		int num1 = 30;
+		int num2 = 20;
+		
+		System.out.println(num1 > num2);  // ture
+		System.out.println(num1 < num2);  // false
+		System.out.println(num1 >= num2); // ture
+		System.out.println(num1 <= num2); // false
+		System.out.println(num1 == num2); // false
+		System.out.println(num1 != num2); // ture
+		
+		if (num1 > num2) {
+			// 참일 때 처리되는 블록
+			System.out.println("num1dl num2보다 큽니다.");
+			
+		}
+	}
+}
+
