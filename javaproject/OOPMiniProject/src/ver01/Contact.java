3dba2fe9fb5b4a592d9a7a92b428da0c5652a21a
ByXavi33 <byxavi33@gmail.com>
2022-09-21 10:50:33 +0900

    프로젝트1
    프로젝트1

---
+++ javaproject/OOPMiniProject/src/ver01/Contact.java
@@ -0,0 +1,100 @@
+package ver01;
+
+//Contact 클래스의 용도 ? => 데이터 저장
+// 인스턴스 생성 - 변수의 메모리 생성 -> 데이터 저장
+public class Contact {
+
+	// 변수들은 직접 참조를 막아 캡슐화 처리
+	// 이름
+	private String name;
+	// 전화번호
+	private String pn;
+	// 이메일
+	private String email;
+	// 주소
+	private String adr;
+	// 생일
+	private int bir;
+	// 그룹
+	private String group;
+
+	// 변수의 직접 참조는 안되지만 변수의 값을 얻을 수 있는 메소드(getter)
+	public String getName() {
+		return name;
+	}
+
+	public String getPn() {
+		return pn;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getAdr() {
+		return adr;
+	}
+
+	public int getBir() {
+		return bir;
+	}
+
+	public String getGroup() {
+		return group;
+	}
+
+	// 변수에 값을 저장할 수 있는 메소드(setter)
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setPn(String pn) {
+		this.pn = pn;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setAdr(String adr) {
+		this.adr = adr;
+	}
+
+	public void setBir(int bir) {
+		this.bir = bir;
+	}
+
+	public void setGroup(String group) {
+		this.group = group;
+	}
+
+	//
+	public Contact(String name, String pn, String email, String adr, int bir, String group) {
+		super();
+		this.name = name;
+		this.pn = pn;
+		this.email = email;
+		this.adr = adr;
+		this.bir = bir;
+		this.group = group;
+	}
+
+	//
+	public void showContactInfo() {
+		System.out.println("_____________________");
+		System.out.println("   이름 : " + this.name);
+		System.out.println("전화번호 : " + this.pn);
+		System.out.println("  이메일 : " + this.email);
+		System.out.println("   주소 : " + this.adr);
+		System.out.println("   생일 : " + this.bir);
+		System.out.println("   그룹 : " + this.group);
+		System.out.println("_____________________");
+
+	}
+
+	@Override
+	public String toString() {
+		return "Contact [name=" + name + ", pn=" + pn + ", email=" + email + ", adr=" + adr + ", bir=" + bir
+				+ ", group=" + group + "]";
+	}
+}
---
+++ javaproject/OOPMiniProject/src/ver01/ContactMain.java
@@ -0,0 +1,53 @@
+package ver01;
+
+import java.util.Scanner;
+
+public class ContactMain {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		String name;// 이름
+		String pn;// 전화번호
+		String email;// 이메일
+		String adr;// 주소
+		int bir;// 생일
+		String group;// 그룹
+
+		while (true) {
+			System.out.println(" 시작 | 종료");
+			String select = sc.nextLine();
+
+			// 사용자 요청에 따라 프로그램 종료/진행 => 분기
+			if (select.equals("종료")) {
+				System.out.println("프로그램을 종료합니다.");
+				break;
+			} else if (!select.equals("시작")) {
+				System.out.println("올바른 요청이 아닙니다.!");
+				continue;
+			}
+
+			System.out.println("데이터 입력을 시작합니다.");
+			System.out.println("________________");
+			System.out.print("이름을 입력해 주세요-->");
+			name = sc.nextLine();
+			System.out.print("전화번호를 입력해 주세요-->");
+			pn = sc.nextLine();
+			System.out.print("이메일을 입력해 주세요-->");
+			email = sc.nextLine();
+			System.out.print("주소를 입력해 주세요-->");
+			adr = sc.nextLine();
+			System.out.print("생일을 입력해 주세요-->");
+			bir = sc.nextInt();
+			sc.nextLine();
+			System.out.print("그룹을 입력해 주세요-->");
+			group = sc.nextLine();
+
+			Contact con = new Contact(name, pn, email, adr, bir, group);
+
+			con.showContactInfo();
+			System.out.println("계속 하시려면 시작을 입력해주시고 그만하시려면 입력를 입력해 주세요");
+		}
+	}
+}
