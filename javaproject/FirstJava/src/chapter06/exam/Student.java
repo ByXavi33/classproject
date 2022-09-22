9c690d1495dccb5a38b886cac253b685e8f73a54
ByXavi33 <byxavi33@gmail.com>
2022-09-22 09:13:38 +0900

    프로젝트
    배열

--- javaproject/FirstJava/src/chapter06/exam/Student.java
+++ javaproject/FirstJava/src/chapter06/exam/Student.java
@@ -3,55 +3,55 @@ package chapter06.exam;
 public class Student {
 
 	private String name;
-	private int korScore;
-	private int engScore;
-	private int matScore;
+	private int kor;
+	private int eng;
+	private int mat;
 
-	public Student(String name, int korScore, int engScore, int matScore) {
+	public Student(String name, int kor, int eng, int mat) {
 		super();
 		this.name = name;
-		this.korScore = korScore;
-		this.engScore = engScore;
-		this.matScore = matScore;
+		this.kor = kor;
+		this.eng = eng;
+		this.mat = mat;
 	}
 
 	public String getName() {
 		return name;
 	}
 
-	public int getKorScore() {
-		return korScore;
+	public int getKor() {
+		return kor;
 	}
 
-	public int getEngScore() {
-		return engScore;
+	public int getEng() {
+		return eng;
 	}
 
-	public int getMatScore() {
-		return matScore;
+	public int getMat() {
+		return mat;
 	}
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-	public void setKorScore(int korScore) {
-		this.korScore = korScore;
+	public void setKor(int kor) {
+		this.kor = kor;
 	}
 
-	public void setEngScore(int engScore) {
-		this.engScore = engScore;
+	public void setEng(int eng) {
+		this.eng = eng;
 	}
 
-	public void setMatScore(int matScore) {
-		this.matScore = matScore;
+	public void setMat(int mat) {
+		this.mat = mat;
 	}
 
 	// 총점을 구해서 반환하는 메소드
 	public int sum() {
 		int result = 0;
 
-		result = korScore + engScore + matScore;
+		result = kor + eng + mat;
 
 		return result;
 	}
@@ -65,21 +65,20 @@ public class Student {
 	// 행 단위 출력
 	public void printData() {
 		System.out.print(this.name + "\t" + 
-	                     this.korScore + "\t" +
-				         this.engScore + "\t" +
-	                     this.matScore + "\t" +
-				         sum() + "\t" +
-	                     avg() + "\n"
-	                     );
+	                     this.kor + "\t" +
+				         this.eng + "\t" +
+	                     this.mat + "\n"
+	             		);
+	                    
 
 	}
 
 	public static void main(String[] args) {
 		
-		Student s = new Student("학생1", 100, 90,80);
+		Student s = new Student("손흥민", 88, 75,62);
 		
 		System.out.println("합 :" +s.sum());
-		System.out.println("평균 " + s.avg());
+		System.out.println("평균 " +s.avg());
 		
 		s.printData();
 		
--- javaproject/FirstJava/src/chapter06/exam/StudentMain.java
+++ javaproject/FirstJava/src/chapter06/exam/StudentMain.java
@@ -5,34 +5,32 @@ public class StudentMain {
 	public static void main(String[] args) {
 
 		// 학생 10명의 정보를 저장하는 배열
-		Student[] students = new Student[10];
+		Student[] students = new Student[5];
 
 		// 점수 초기화
 		for (int i = 0; i < students.length; i++) {
 
-			students[i] = new Student("학생" + i, (int) (Math.random() * 51) + 50, (int) (Math.random() * 51) + 50,
-					(int) (Math.random() * 51) + 50);
+			students[i] = new Student("학생" + i, 
+					           (int) (Math.random()* 81) + 20,
+					           (int) (Math.random() * 81) + 20,
+					           (int) (Math.random() * 81) + 20
+					           );
 		}
-
-		int engTotalScore = 0;
-		int korTotalScore = 0;
-		int matTotalScore = 0;
+		int engTotal = 0;
+		int korTotal = 0;
+		int matTotal = 0;
 
 		System.out.println("성적표");
-		System.out.println("------------------------------------------------------------------");
-		System.out.println("이름\t국어\t영어\t수학\t총점\t평균\t총점\t");
-		System.out.println("------------------------------------------------------------------");
+		System.out.println("----------------------------------------------");
+		System.out.println("이름\t국어\t영어\t수학\t");
+		System.out.println("----------------------------------------------");
 
 		for (Student s : students) {
 			s.printData();
-			korTotalScore += s.getKorScore();
-			engTotalScore += s.getEngScore();
-			matTotalScore += s.getEngScore();
+			korTotal += s.getKor();
+			engTotal += s.getEng();
+			matTotal += s.getMat();
 		}
-		System.out.println("------------------------------------------------------------------");
-		System.out.printf("과목 총점\t %d \t %d\t %d \n",korTotalScore,engTotalScore,matTotalScore);
-		
-		System.out.printf("과목 평균 \t %.1f \t %.1f \t %.1f \n",korTotalScore/10f,engTotalScore/10f,matTotalScore/10f);
-		System.out.println("------------------------------------------------------------------");
 	}
 }
+		
