c2a6e8902558cc0a517be37eddaddbfd85b5b16f
ByXavi33 <byxavi33@gmail.com>
2022-09-22 09:15:47 +0900

    프로젝트
    배열

--- javaproject/FirstJava/src/chapter06/exam/StudentMain.java
+++ javaproject/FirstJava/src/chapter06/exam/StudentMain.java
@@ -4,14 +4,14 @@ public class StudentMain {
 
 	public static void main(String[] args) {
 
-		// 학생 10명의 정보를 저장하는 배열
+		// 학생 5명의 정보를 저장하는 배열
 		Student[] students = new Student[5];
 
 		// 점수 초기화
 		for (int i = 0; i < students.length; i++) {
 
 			students[i] = new Student("학생" + i, 
-					           (int) (Math.random()* 81) + 20,
+					           (int) (Math.random()* 81) + 20, //20점 이상 점수 구하기
 					           (int) (Math.random() * 81) + 20,
 					           (int) (Math.random() * 81) + 20
 					           );
