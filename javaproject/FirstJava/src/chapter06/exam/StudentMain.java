230837ba8d1b83d74292ed1e89429d0920f507e6
ByXavi33 <byxavi33@gmail.com>
2022-09-27 09:34:54 +0900

    Update StudentMain.java

--- javaproject/FirstJava/src/chapter06/exam/StudentMain.java
+++ javaproject/FirstJava/src/chapter06/exam/StudentMain.java
@@ -30,6 +30,11 @@ public class StudentMain {
 			korTotal += s.getKor();
 			engTotal += s.getEng();
 			matTotal += s.getMat();
+			
+			System.out.println("--------------------------------------------");
+			System.out.printf("과목총점\t %d \t %d \t %d \n", korTotal, engTotal, matTotal);
+			System.out.printf("과목평균\t %.1f \t%.1f \t %.1f \n", korTotal / 10f, engTotal / 10f,
+					matTotal / 10f);
 		}
 	}
 }
